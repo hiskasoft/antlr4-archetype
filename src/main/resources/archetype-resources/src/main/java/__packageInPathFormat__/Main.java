9d2171ce91be15bd44229a7def41d01c2dfc81b8
Willyams Yujra <yracnet@gmail.com>
2021-04-30 12:03:23 -0400

    + test files

--- src/main/resources/archetype-resources/src/main/java/__packageInPathFormat__/Main.java
+++ src/main/resources/archetype-resources/src/main/java/__packageInPathFormat__/Main.java
@@ -10,24 +10,26 @@ import org.antlr.v4.runtime.CommonTokenStream;
 
 public class Main {
 
-	private static final String EXTENSION = "${fileExtension}";
+    private static final String EXTENSION = "${fileExtension}";
+    private static final String DIRBASE = "src/test/resources/";
+    
 
-	public static void main(String[] args) throws IOException {
-		String program = args.length > 1 ? args[1] : "src/test/resources/test." + EXTENSION;
+    public static void main(String[] args) throws IOException {
+        String files[] = args.length==0? new String[]{ "test." + EXTENSION } : args;
+        System.out.println("Process dirbase: " + DIRBASE);
+        for (String file : files){
+            System.out.println("Interpreting file " + file);
 
-		System.out.println("Interpreting file " + program);
+            ${grammarName}Lexer lexer = new ${grammarName}Lexer(new ANTLRFileStream( DIRBASE + file));
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            ${grammarName}Parser parser = new ${grammarName}Parser(tokens);
+            ${grammarName}Parser.StartContext tree = parser.start();
+            ${grammarName}CustomVisitor visitor = new ${grammarName}CustomVisitor();
+            visitor.visit(tree);
 
-		${grammarName}Lexer lexer = new ${grammarName}Lexer(new ANTLRFileStream(program));
-		CommonTokenStream tokens = new CommonTokenStream(lexer);
-		${grammarName}Parser parser = new ${grammarName}Parser(tokens);
+            System.out.println("Interpretation finished");
+        }
 
-		${grammarName}Parser.StartContext tree = parser.start();
-
-		${grammarName}CustomVisitor visitor = new ${grammarName}CustomVisitor();
-		visitor.visit(tree);
-
-		System.out.println("Interpretation finished");
-
-	}
+    }
 
 }
