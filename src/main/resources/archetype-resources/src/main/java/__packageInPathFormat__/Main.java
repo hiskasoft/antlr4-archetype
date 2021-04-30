b6083ff09a103abb1378dcf2d2ec46a4bdf27a4c
Willyams Yujra <yracnet@gmail.com>
2021-04-30 12:17:46 -0400

    + format

--- src/main/resources/archetype-resources/src/main/java/__packageInPathFormat__/Main.java
+++ src/main/resources/archetype-resources/src/main/java/__packageInPathFormat__/Main.java
@@ -1,35 +1,33 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-
 package ${package};
-import java.io.IOException;
 
-import org.antlr.v4.runtime.ANTLRFileStream;
+import java.io.IOException;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 
 public class Main {
 
     private static final String EXTENSION = "${fileExtension}";
     private static final String DIRBASE = "src/test/resources/";
-    
 
     public static void main(String[] args) throws IOException {
         String files[] = args.length==0? new String[]{ "test." + EXTENSION } : args;
-        System.out.println("Process dirbase: " + DIRBASE);
+        System.out.println("Dirbase: " + DIRBASE);
         for (String file : files){
-            System.out.println("Interpreting file " + file);
+            System.out.println("START: " + file);
 
-            ${grammarName}Lexer lexer = new ${grammarName}Lexer(new ANTLRFileStream( DIRBASE + file));
+            CharStream in = CharStreams.fromFileName(DIRBASE + file);
+            ${grammarName}Lexer lexer = new ${grammarName}Lexer(in);
             CommonTokenStream tokens = new CommonTokenStream(lexer);
             ${grammarName}Parser parser = new ${grammarName}Parser(tokens);
             ${grammarName}Parser.StartContext tree = parser.start();
             ${grammarName}CustomVisitor visitor = new ${grammarName}CustomVisitor();
             visitor.visit(tree);
 
-            System.out.println("Interpretation finished");
+            System.out.println("FINISH: " + file);
         }
-
     }
-
 }
